4f27105517f6d9b2fdf7f88aa702ec88f4500ad6
kalon <hansick001@gmail.com>
2016-08-31 21:54:50 +0900

    new id check

--- php code/idcheck.php
+++ php code/idcheck.php
@@ -1,5 +1,33 @@
 <?php
 
+ // $conn = mysqli_connect("222.116.135.137", "windadmin", "project06", "wind");
+  include_once 'db_conn.php';
 
+  $id =$_POST['id'];
+
+  if(mysqli_connect_errno())
+  {
+    echo "Fail to connet to mysql : ", mysqli_connect_errno();
+  }
+
+  mysqli_query($conn, "set session character_set_connection=UTF-8;");
+
+  $sql = "select count(*) cnt from user where id = '".$id."';";
+  $result = mysqli_query($conn, $sql) or die("[{\"success\":\"3\"}]");        //success : 3 Db연결오류   
+  mysqli_data_seek($result,0);
+  $row = mysqli_fetch_array($result);
+  $total_record = $row['cnt'];
+
+  //already enrolled address
+  if($total_record > 0 )
+  {
+    echo "{\"Success\":\"2\"}";
+  }
+  else if($total_record == 0)
+  {
+     echo "{\"Success\":\"1\"}";
+  }
+ 
+  
 
 ?>

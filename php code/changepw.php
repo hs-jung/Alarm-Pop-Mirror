343b780e219c6f52847fb9088930d83de6f949de
kalon <hansick001@gmail.com>
2016-09-02 14:02:34 +0900

    password 일치 여부 확인

--- php code/changepw.php
+++ php code/changepw.php
@@ -13,10 +13,27 @@
 
   mysqli_query($conn, "set session character_set_connection=UTF-8;");
 
- $sql = "update user set pw = PASSWORD('".$new_pw."') where uuid = '".$id."' and pw = PASSWORD('".$pw."');";
- $result = mysqli_query($conn, $sql) or die("[{\"success\":\"3\"}]");
+  $sql = "select count(*) cnt from user where uuid = '".$id."' and pw = PASSWORD('".$pw."');";
+  $result = mysqli_query($conn, $sql) or die("[{\"Success\":\"3\"}]");        //success : 3 Db연결오류   
+  mysqli_data_seek($result,0);
+  $row = mysqli_fetch_array($result);
+  $total_record = $row['cnt'];
 
- echo "{\"Success\":\"1\"}";
+  //already enrolled address
+  if($total_record > 0 )
+  {
+    $sql1 = "update user set pw = PASSWORD('".$new_pw."') where uuid = '".$id."' and pw = PASSWORD('".$pw."');";
+    $result1 = mysqli_query($conn, $sql1) or die("[{\"success\":\"3\"}]");
+    echo "{\"Success\":\"1\"}";
+  }
+  else if($total_record == 0)
+  {
+     echo "{\"Success\":\"2\"}";
+  }
+ 
+  
+
+ 
 
 
 ?>
